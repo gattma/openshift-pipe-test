f3b062db3e4c788a96d70b2dbd499bbb843eecfa
gattma <s1810454012@students.fh-hagenberg.at>
2019-06-25 16:20:20 +0200

    test

--- ParkingManagerParkingService/src/main/java/at/fh/sve/util/Constants.java
+++ ParkingManagerParkingService/src/main/java/at/fh/sve/util/Constants.java
@@ -8,7 +8,7 @@ public class Constants {
 
         public static final String DEFAULT_DATASTORE_PROVIDER = "mongodb";
         public static final String DEFAULT_DATABASE = "parkingDB";
-        public static final String DEFAULT_DB_HOST = "localhost";
+        public static final String DEFAULT_DB_HOST = "mongodb.test.svc";
         public static final String DEFAULT_DB_PORT = "27017";
 
         public static final String CONFIG_PROP_DATASTORE_PROV = "DATASTORE_PROVIDER";
