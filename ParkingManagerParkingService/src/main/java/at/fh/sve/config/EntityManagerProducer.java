cdea3561a89e02662f4b4bfbc45622dcce1751d3
gattma <s1810454012@students.fh-hagenberg.at>
2019-06-25 16:44:53 +0200

    test

--- ParkingManagerParkingService/src/main/java/at/fh/sve/config/EntityManagerProducer.java
+++ ParkingManagerParkingService/src/main/java/at/fh/sve/config/EntityManagerProducer.java
@@ -48,6 +48,8 @@ public class EntityManagerProducer {
             put(MongoDBProperties.HOST, DB_HOST);
             put(MongoDBProperties.PORT, DB_PORT);
             put(MongoDBProperties.CREATE_DATABASE, "true");
+            put(MongoDBProperties.USERNAME, "sve");
+            put(MongoDBProperties.PASSWORD, "sve");
         }};
     }
 
